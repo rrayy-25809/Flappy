af19f358cd11c4e51914ea7d4fbf24f29f6765f4
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-11-18 16:58:50 +0900

    Changes to be committed: 새의 점프 기능 구현 	modified:   Assets/Jump.cs 	modified:   ProjectSettings/ProjectSettings.asset 	modified:   UserSettings/Layouts/default-6000.dwlt

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class Jump : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class Jump : MonoBehaviour { // MonoBehaviour 상속
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // 프레임이 업데이트 될 때마다 호출되는 메서드
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) { // 마우스 왼쪽 버튼 클릭 감지
+            rb.linearVelocity = Vector2.up * 3; // (0.3) 점프
+        }
     }
 }
